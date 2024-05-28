5e992ff3759e8fa1ba724c1f6d5145754817ef61
LKEthridge <168391899+LKEthridge@users.noreply.github.com>
2024-05-27 20:47:58 -0500

    Project Completed?

---
+++ App.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+cars = pd.read_csv('vehicles_us.csv')
+cars['make'] = cars['model'].apply(lambda x: x.split()[0])
+st.header('Data Viewer')
+st.dataframe(cars)
+st.header('Vehicle Types by Make')
+fig1 = px.histogram(cars, x='make', color='type')
+st.write(fig1)
+st.header('Histogram of Condition vs Model Year')
+fig2 = px.histogram(cars, x='model_year', color='condition')
+st.write(fig2)
+st.header('Compare Price Distribution between Makes')
+make_list = sorted(cars['make'].unique())
+make_1 = st.selectbox(
+    label = 'Select Make 1',
+    options=make_list,
+    index=make_list.index('honda')
+)
+make_2 = st.selectbox(
+    label='Select Make 2',
+    options=make_list,
+    index=make_list.index('toyota')
+)
+mask_filter = (cars['make'] == make_1) | (cars['make'] == make_2)
+cars_filtered = cars[mask_filter]
+normalize = st.checkbox('Normalize Histogram', value=True, key=1)
+if normalize:
+    histnorm = 'percent'
+else:
+    histnorm = None
+fig3 = px.histogram(cars_filtered,
+                   x='price',
+                   nbins=30,
+                   color='make',
+                   histnorm=histnorm,
+                   barmode='overlay')
+st.write(fig3)
+st.header('Average Cost by Make')
+cars_ppm = cars.groupby('make').agg(avg_price=('price', 'mean')).reset_index()
+fig4 = px.bar(cars_ppm, x='make', y='avg_price')
+st.write(fig4)
+st.header('Price Distribution by Make (All)')
+fig5 = px.box(cars, x='price', y='make')
+st.write(fig5)
+st.header('Average Cost by Model Year')
+cars_ppmy = cars.groupby('model_year').agg(avg_price=('price', 'mean')).reset_index()
+fig6 = px.scatter(cars_ppmy, x='model_year', y='avg_price')
+st.write(fig6)
+st.header('Cost by Condition')
+normalize = st.checkbox('Normalize Histogram', value=True, key=2)
+if normalize:
+    histnorm = 'percent'
+else:
+    histnorm = None
+fig7 = px.histogram(cars_filtered,
+                    x='price',
+                    nbins=30,
+                    color='condition',
+                    histnorm=histnorm,
+                    barmode='overlay')
+st.write(fig7)
---
+++ README.md
@@ -0,0 +1,5 @@
+# SDT_Project
+#This is a project for TripleTen, Sprint 4!
+#This project visualizes car advertisement data from one data set containing information on over 50k cars available for sale.  It includes interactive visuals for exploring the number of vehicle types by make, condition vs model year, comparing the price distribution between two makes, the average cost by make, the price distribution by make for all makes, average cost by model year, and cost by condition.
+#This project uses pandas, streamlit, plotly-express, and matplotlib.  It requires python 3.11.
+#To run the project on your local machine, type "streamlit run app.py" into your terminal.

6c46afffe5c02868562d308490b7cf0b298da4f8
LKEthridge <168391899+LKEthridge@users.noreply.github.com>
2024-05-31 23:26:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,44 @@
 import streamlit as st
 import pandas as pd
 import plotly_express as px
+
+#import and clean data
 cars = pd.read_csv('vehicles_us.csv')
 cars['make'] = cars['model'].str.split().str[0]
-st.header('Data Viewer')
+cars['is_4wd'] = cars['is_4wd'].fillna('0')
+cars['model_year'] = cars[['model_year', 'model']].groupby('model').transform(lambda x:x.fillna(x.median()))
+cars['cylinders'] = cars[['cylinders', 'model']].groupby('model').transform(lambda x:x.fillna(x.median()))
+cars['odometer'] = cars[['odometer', 'condition']].groupby('condition').transform(lambda x:x.fillna(x.mean()))
+cars = cars.fillna('Unknown')
+
+#figure 1
+st.header('Figure 1: Data Viewer')
 st.dataframe(cars)
 st.header('Vehicle Types by Make')
 fig1 = px.histogram(cars, x='make', color='type')
 st.write(fig1)
-st.header('Histogram of Condition vs Model Year')
+
+#figure 2
+st.header('Figure 2: Histogram of Condition vs Model Year')
 fig2 = px.histogram(cars, x='model_year', color='condition')
 st.write(fig2)
-st.header('Compare Price Distribution between Makes')
+
+#figure 3
+st.header('Figure 3: Compare Price Distribution between Makes')
 make_list = sorted(cars['make'].unique())
 make_1 = st.sidebar.selectbox(
-    label = 'Select Make 1',
+    label = 'Figure 3: Select Make 1',
     options=make_list,
     index=make_list.index('honda')
 )
 make_2 = st.sidebar.selectbox(
-    label='Select Make 2',
+    label='Figure 3: Select Make 2',
     options=make_list,
     index=make_list.index('toyota')
 )
 mask_filter = (cars['make'] == make_1) | (cars['make'] == make_2)
 cars_filtered = cars[mask_filter]
-normalize = st.sidebar.checkbox('Normalize Histogram', value=True, key=1)
+normalize = st.sidebar.checkbox('Normalize Histogram for Figure 3', value=True, key=1)
 if normalize:
     histnorm = 'percent'
 else:
@@ -37,19 +50,27 @@ fig3 = px.histogram(cars_filtered,
                    histnorm=histnorm,
                    barmode='overlay')
 st.write(fig3)
-st.header('Average Cost by Make')
+
+#figure 4
+st.header('Figure 4: Average Cost by Make')
 cars_ppm = cars.groupby('make').agg(avg_price=('price', 'mean')).reset_index()
 fig4 = px.bar(cars_ppm, x='make', y='avg_price')
 st.write(fig4)
-st.header('Price Distribution by Make (All)')
+
+#figure 5
+st.header('Figure 5: Price Distribution by Make (All)')
 fig5 = px.box(cars, x='price', y='make')
 st.write(fig5)
-st.header('Average Cost by Model Year')
+
+#figure 6
+st.header('Figure 6: Average Cost by Model Year')
 cars_ppmy = cars.groupby('model_year').agg(avg_price=('price', 'mean')).reset_index()
 fig6 = px.scatter(cars_ppmy, x='model_year', y='avg_price')
 st.write(fig6)
-st.header('Cost by Condition')
-normalize = st.sidebar.checkbox('Normalize Histogram', value=True, key=2)
+
+#figure 7
+st.header('Figure 7: Cost by Condition')
+normalize = st.sidebar.checkbox('Normalize Histogram for Figure 7', value=True, key=2)
 if normalize:
     histnorm = 'percent'
 else:
